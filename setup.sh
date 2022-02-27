50bb5f0631b4c107eff79c0aee564b7b4eb918dc
maxtremblay <m@xtremblay.ca>
2022-02-27 13:44:11 -0500

    initial setup with zsh

--- setup.sh
+++ setup.sh
@@ -25,15 +25,15 @@ sudo dnf install -y \
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/powerlevel10k
 
 # Install Meslo patched nerd font
-mkdir -p '/home/maxime/.local/share/fonts/MesloLGS NF'
+mkdir -p '/home/maxime/.local/share/fonts/meslo'
 curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
-    -o '/home/maxime/.local/share/fonts/MesloLGS NF/MesloLGS NF Regular.ttf'
+    -o '/home/maxime/.local/share/fonts/meslo/MesloLGS_NF_Regular.ttf'
 curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
-    -o '/home/maxime/.local/share/fonts/MesloLGS NF/MesloLGS NF Bold.ttf'
+    -o '/home/maxime/.local/share/fonts/meslo/MesloLGS_NF_Bold.ttf'
 curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
-    -o '/home/maxime/.local/share/fonts/MesloLGS NF/MesloLGS NF Italic.ttf'
+    -o '/home/maxime/.local/share/fonts/meslo/MesloLGS_NF_Italic.ttf'
 curl https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf \
-    -o '/home/maxime/.local/share/fonts/MesloLGS NF/MesloLGS NF Bold Italic.ttf'
+    -o '/home/maxime/.local/share/fonts/meslo/MesloLGS_NF_Bold_Italic.ttf'
 
 fc-cache -v # update the font cache
 
